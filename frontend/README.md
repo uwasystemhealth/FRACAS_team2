89316142f713697d94adf164e52d65fd695b5183
Insan Basrewan <57213980+insanbas@users.noreply.github.com>
2023-08-24 17:49:43 +0800

    Initial Page Prototype

--- frontend/README.md
+++ frontend/README.md
@@ -1,34 +1,34 @@
-This is a [Next.js](https://nextjs.org/) project bootstrapped with [`create-next-app`](https://github.com/vercel/next.js/tree/canary/packages/create-next-app).
+# Material UI - Next.js App Router example in TypeScript
 
-## Getting Started
+This is a [Next.js](https://nextjs.org/) project bootstrapped using [`create-next-app`](https://github.com/vercel/next.js/tree/canary/packages/create-next-app) with Material UI installed.
 
-First, run the development server:
+## How to use
+
+Download the example [or clone the repo](https://github.com/mui/material-ui):
+
+<!-- #default-branch-switch -->
 
 ```bash
+curl https://codeload.github.com/mui/material-ui/tar.gz/master | tar -xz --strip=2  material-ui-master/examples/material-ui-nextjs-ts
+cd material-ui-nextjs-ts
+```
+
+Install dependencies and run:
+
+```bash
+npm install
 npm run dev
-# or
-yarn dev
-# or
-pnpm dev
 ```
 
 Open [http://localhost:3000](http://localhost:3000) with your browser to see the result.
 
-You can start editing the page by modifying `app/page.tsx`. The page auto-updates as you edit the file.
+You can start editing the page by modifying `src/app/page.tsx`. The page auto-updates as you edit the file.
 
-This project uses [`next/font`](https://nextjs.org/docs/basic-features/font-optimization) to automatically optimize and load Inter, a custom Google Font.
+This example uses [`next/font/google`](https://nextjs.org/docs/app/building-your-application/optimizing/fonts#google-fonts) to automatically optimize and load Roboto, a custom Google Font.
 
-## Learn More
+## Learn more
 
-To learn more about Next.js, take a look at the following resources:
+To learn more about this example:
 
 - [Next.js Documentation](https://nextjs.org/docs) - learn about Next.js features and API.
-- [Learn Next.js](https://nextjs.org/learn) - an interactive Next.js tutorial.
-
-You can check out [the Next.js GitHub repository](https://github.com/vercel/next.js/) - your feedback and contributions are welcome!
-
-## Deploy on Vercel
-
-The easiest way to deploy your Next.js app is to use the [Vercel Platform](https://vercel.com/new?utm_medium=default-template&filter=next.js&utm_source=create-next-app&utm_campaign=create-next-app-readme) from the creators of Next.js.
-
-Check out our [Next.js deployment documentation](https://nextjs.org/docs/deployment) for more details.
+- [Customizing Material UI](https://mui.com/material-ui/customization/how-to-customize/) - approaches to customizing Material UI.
---
+++ frontend/src/app/createreport/page.tsx
@@ -0,0 +1,337 @@
+'use client'
+import React, { FC } from 'react';
+import { TextField } from '@mui/material/';
+import { useForm, SubmitHandler, Controller } from 'react-hook-form';
+import { yupResolver } from "@hookform/resolvers/yup"
+import * as yup from "yup"
+
+import Box from '@mui/material/Box';
+import Stepper from '@mui/material/Stepper';
+import Step from '@mui/material/Step';
+import StepLabel from '@mui/material/StepLabel';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+import Grid from '@mui/material/Unstable_Grid2';
+import MenuItem from '@mui/material/MenuItem';
+import Select, { SelectChangeEvent } from '@mui/material/Select';
+import InputLabel from '@mui/material/InputLabel';
+import FormControl from '@mui/material/FormControl';
+import Divider from '@mui/material/Divider';
+
+
+const steps = ['Basic Info', 'Additional Info', 'Review'];
+
+interface IFormInputs {
+  failure_title: string;
+  description: string;
+  impact: string;
+  cause: string;
+  mechanism: string;
+  corrective_action: string;
+  subsystem: string;
+  car_year: number;
+  team_id: number;
+  failure_time: string;
+}
+
+const schema = yup.object().shape({
+  failure_title: yup.string().required(),
+  description: yup.string().min(5).required(),
+  impact: yup.string(),
+  cause: yup.string(),
+  mechanism: yup.string(),
+  corrective_action: yup.string(),
+  subsystem: yup.string().required(),
+  car_year: yup.number(),
+  team_id: yup.number().required(),
+  failure_time: yup.string().required()
+});
+
+export default function CreateReport() {
+  const [team, setTeam] = React.useState('');
+  const [subsystem, setSubsystem] = React.useState('');
+
+  const teamChange = (event: SelectChangeEvent) => {
+    setTeam(event.target.value as string);
+  };
+
+  const subsystemChange = (event: SelectChangeEvent) => {
+    setSubsystem(event.target.value as string);
+  };
+
+  const [activeStep, setActiveStep] = React.useState(0);
+
+  const handleNext = () => {
+    setActiveStep((prevActiveStep) => prevActiveStep + 1);
+  };
+
+  const handleBack = () => {
+    setActiveStep((prevActiveStep) => prevActiveStep - 1);
+  };
+
+  const {
+    register,
+    control,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm<IFormInputs>({
+    resolver: yupResolver(schema),
+  });
+
+  const onSubmit: SubmitHandler<IFormInputs> = (data) => console.log('data submitted: ', data);
+
+  console.log(watch('email'));
+  console.log('errors are', errors);
+
+  return (
+    <Box sx={{ width: '100%' }}>
+      <Stepper activeStep={activeStep}>
+        {steps.map((label, index) => {
+          const stepProps: { completed?: boolean } = {};
+          const labelProps: {
+            optional?: React.ReactNode;
+          } = {};
+          return (
+            <Step key={label} {...stepProps}>
+              <StepLabel {...labelProps}>{label}</StepLabel>
+            </Step>
+          );
+        })}
+      </Stepper>
+      {activeStep === steps.length ? (
+        <React.Fragment>
+          <Typography sx={{ mt: 2, mb: 1 }}>
+            All steps completed - you&apos;re finished
+          </Typography>
+          <Box sx={{ display: 'flex', flexDirection: 'row', pt: 2 }}>
+            <Box sx={{ flex: '1 1 auto' }} />
+          </Box>
+        </React.Fragment>
+      ) : (
+        <React.Fragment>
+          <form onSubmit={handleSubmit(onSubmit)}>
+          {(activeStep === 0 || activeStep === 2) && (
+            <Box sx={{ flexGrow: 1, py: 2 }}>
+              <Grid container spacing={2}>
+                <Grid xs={10}>
+                  <Controller
+                  name="failure_title"
+                  control={control}
+                  render={({ field }) => (
+                    <TextField
+                      {...field}
+                      sx={{ py: 4 }}
+                      label="Title"
+                      variant="standard"
+                      error={!!errors.failure_title}
+                      helperText={errors.failure_title ? errors.failure_title?.message : ''}
+                      fullWidth
+                    />
+                  )}
+                />
+                </Grid>
+                <Grid xs={3}>
+                  <Controller
+                  name="team_id"
+                  control={control}
+                  render={({ field }) => (
+                    <FormControl fullWidth>
+                    <InputLabel id="team">Team</InputLabel>
+                    <Select
+                      {...field}
+                      labelId="team"
+                      id="team"
+                      value={team}
+                      label="Team"
+                      onChange={teamChange}
+                    >
+                      <MenuItem value={1}>Powertrain</MenuItem>
+                      <MenuItem value={2}>Suspension</MenuItem>
+                    </Select>
+                    </FormControl>
+                  )}
+                />
+                </Grid>
+                <Grid xs={3}>
+                  <Controller
+                  name="subsystem"
+                  control={control}
+                  render={({ field }) => (
+                    <FormControl fullWidth>
+                    <InputLabel id="subsystem">Subsystem</InputLabel>
+                    <Select
+                      {...field}
+                      labelId="subsystem"
+                      id="subsystem"
+                      value={subsystem}
+                      label="Subsystem"
+                      onChange={subsystemChange}
+                    >
+                      <MenuItem value={1}>PDK</MenuItem>
+                      <MenuItem value={2}>Exhaust</MenuItem>
+                    </Select>
+                    </FormControl>
+                  )}
+                />
+                </Grid>
+                <Divider orientation="vertical" flexItem></Divider>
+                <Grid xs={2}>
+                  <Controller
+                  name="car_year"
+                  control={control}
+                  render={({ field }) => (
+                    <TextField
+                      {...field}
+                      label="Car Year"
+                      variant="outlined"
+                      error={!!errors.failure_title}
+                      helperText={errors.failure_title ? errors.failure_title?.message : ''}
+                      fullWidth
+                    />
+                  )}
+                />
+                </Grid>
+                <Grid xs={2}>
+                  <Controller
+                  name="failure_time"
+                  control={control}
+                  render={({ field }) => (
+                    <TextField
+                      {...field}
+                      label="Time of Failure"
+                      variant="outlined"
+                      error={!!errors.failure_title}
+                      helperText={errors.failure_title ? errors.failure_title?.message : ''}
+                      fullWidth
+                    />
+                  )}
+                />
+                </Grid>
+                <Grid xs={12}>
+                  <Controller
+                  name="description"
+                  control={control}
+                  render={({ field }) => (
+                    <TextField
+                      {...field}
+                      label="Description"
+                      variant="outlined"
+                      error={!!errors.description}
+                      helperText={errors.description ? errors.description?.message : ''}
+                      fullWidth
+                      multiline
+                      minRows={4}
+                    />
+                  )}
+                />
+                </Grid>
+              </Grid>
+            </Box>
+            )}
+            {(activeStep === 1 || activeStep === 2) && (
+              <Box sx={{ flexGrow: 1, py: 4 }}>
+              <Grid container spacing={2}>
+              <Grid xs={12}>
+                  <Controller
+                  name="impact"
+                  control={control}
+                  render={({ field }) => (
+                    <TextField
+                      {...field}
+                      label="Impact"
+                      variant="outlined"
+                      error={!!errors.impact}
+                      helperText={errors.impact ? errors.impact?.message : ''}
+                      fullWidth
+                      multiline
+                      minRows={4}
+                    />
+                  )}
+                />
+                </Grid>
+                <Grid xs={12}>
+                  <Controller
+                  name="cause"
+                  control={control}
+                  render={({ field }) => (
+                    <TextField
+                      {...field}
+                      label="Cause"
+                      variant="outlined"
+                      error={!!errors.cause}
+                      helperText={errors.cause ? errors.cause?.message : ''}
+                      fullWidth
+                      multiline
+                      minRows={4}
+                    />
+                  )}
+                />
+                </Grid>
+                <Grid xs={12}>
+                  <Controller
+                  name="mechanism"
+                  control={control}
+                  render={({ field }) => (
+                    <TextField
+                      {...field}
+                      label="Mechanism"
+                      variant="outlined"
+                      error={!!errors.mechanism}
+                      helperText={errors.mechanism ? errors.mechanism?.message : ''}
+                      fullWidth
+                      multiline
+                      minRows={4}
+                    />
+                  )}
+                />
+                </Grid>
+                <Grid xs={12}>
+                  <Controller
+                  name="corrective_action"
+                  control={control}
+                  render={({ field }) => (
+                    <TextField
+                      {...field}
+                      label="Corrective Action Plan"
+                      variant="outlined"
+                      error={!!errors.corrective_action}
+                      helperText={errors.corrective_action ? errors.corrective_action?.message : ''}
+                      fullWidth
+                      multiline
+                      minRows={4}
+                    />
+                  )}
+                />
+                </Grid>
+              </Grid>
+            </Box>
+            )}
+          </form>
+          <Box sx={{ display: 'flex', flexDirection: 'row', pt: 2 }}>
+            <Button
+              color="inherit"
+              disabled={activeStep === 0}
+              onClick={handleBack}
+              sx={{ mr: 1 }}
+            >
+              Back
+            </Button>
+            <Box sx={{ flex: '1 1 auto' }} />
+            {(activeStep === steps.length - 1) ? (
+              <Button 
+              type='submit' variant="contained">
+              Submit
+            </Button>
+            ) : (
+            <Button 
+              onClick={handleNext} disabled={activeStep === steps.length - 1}>
+              Next
+            </Button>
+            )}
+          </Box>
+        </React.Fragment>
+      )}
+    </Box>
+  );
+};
