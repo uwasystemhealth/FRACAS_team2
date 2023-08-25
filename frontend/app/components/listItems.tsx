623bd309cfae4ad894c1d9214b7aada26cf0c681
cryptographer3 <cryptographer3@users.noreply.github.com>
2023-08-25 13:40:06 +0800

    made some changes

--- frontend/app/components/listItems.tsx
+++ frontend/app/components/listItems.tsx
@@ -4,7 +4,6 @@ import ListItemIcon from '@mui/material/ListItemIcon';
 import ListItemText from '@mui/material/ListItemText';
 import ListSubheader from '@mui/material/ListSubheader';
 import DashboardIcon from '@mui/icons-material/Dashboard';
-import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
 import PeopleIcon from '@mui/icons-material/People';
 import BarChartIcon from '@mui/icons-material/BarChart';
 import LayersIcon from '@mui/icons-material/Layers';
@@ -17,12 +16,6 @@ export const mainListItems = (
         <DashboardIcon />
       </ListItemIcon>
       <ListItemText primary="Dashboard" />
-    </ListItemButton>
-    <ListItemButton>
-      <ListItemIcon>
-        <ShoppingCartIcon />
-      </ListItemIcon>
-      <ListItemText primary="Sample" />
     </ListItemButton>
     <ListItemButton>
       <ListItemIcon>
